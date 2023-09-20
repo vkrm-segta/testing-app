7d88d05cf0e09ea90c1637f9ee9214adfbd57808
Vikram Segta <vikramsegta43@gmail.com>
2023-09-20 08:13:30 +0530

    added splach screen and folder structure

---
+++ screens/HomeScreen.js
@@ -0,0 +1,10 @@
+import { StyleSheet, Text, View } from 'react-native';
+import tw from 'twrnc';
+
+function HomeScreen() {
+    return (
+        <Text style={tw`w-full h-full text-center mt-5`}>Home Screen</Text>
+    )
+}
+
+export default HomeScreen
